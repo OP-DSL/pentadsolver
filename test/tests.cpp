7aed2905a76837a76df414154896ad6ecb79aa73
Daniel Balogh <balogh.dani54@gmail.com>
2022-05-17 09:44:09 +0200

    Fix test/CMakeLists.txt
      Link to pentadsolver insted of myproject
      remove constexpr tests

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,9 +1,6 @@
 #include <catch2/catch.hpp>
 
-unsigned int Factorial(unsigned int number)// NOLINT(misc-no-recursion)
-{
-  return number <= 1 ? number : Factorial(number - 1) * number;
-}
+#include "pentadsolver.hpp"
 
 TEST_CASE("Factorials are computed", "[factorial]")
 {
