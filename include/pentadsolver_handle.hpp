8093a722d1404474a76c2843b659ae37d3f93dfe
Daniel Balogh <balogh.dani54@gmail.com>
2022-07-18 21:27:48 +0200

    add missing files from previous commit

---
+++ include/pentadsolver_handle.hpp
@@ -0,0 +1,11 @@
+#ifndef PENTADSOLVER_HANDLE_HPP_INCLUDED
+#define PENTADSOLVER_HANDLE_HPP_INCLUDED
+
+#ifndef PENTADSOLVER_MPI
+using pentadsolver_handle_t = int *;
+#else
+using pentadsolver_handle_t = int *;
+// TODO
+#endif /* ifndef PENTADSOLVER_MPI */
+
+#endif /* ifndef PENTADSOLVER_HANDLE_HPP_INCLUDED */
